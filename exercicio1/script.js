0f3c3a92b1f71689098adf04d800788b51bf03c4
Elenilson <elenilsonsantos9@gmail.com>
2022-07-19 18:57:15 -0300

    exercicio feito

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+// criando a variável idade e nome.(letra a e b)
+
+const nome = "elenilson";
+
+let idade = "29";
+
+// imprimindo o tipo de variavel. (letra c)
+
+console.log(typeof nome, typeof idade)
+
+// variável foi impressa dessa forma porque a função typeof mostra o tipo de variável que o usuário demonstrou. (letra d)
+
+// perguntando ao usuário qual o seu nome e idade (letra e)
+
+prompt ('qual o seu nome? e sua idade?')
+
+console.log (nome, idade)
+
+// imprimindo novamente o tipo de variável (letra f)
+
+console.log(typeof nome, typeof idade)
+
+// imprimindo mensagem utilizando as variáveis (letra g)
+console.log('olá', nome, 'você tem', idade, 'anos')
--- exercicio3/README.md
+++ exercicio3/README.md
@@ -5,7 +5,6 @@ Suponha que temos duas variáveis `a` e `b`, cada uma com um valor inicial
 ```jsx
 let a = 10
 let b = 25
-```
 
 Agora, queremos trocar os valores delas, de forma que `a` passe a ter o valor de `b` e `b` passe a ter o valor de `a`. 
 
